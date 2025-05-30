8f890070d1ee6bffb15a4668731e2d5de6320478
Yonas Mulyadi <dev.yonas15@gmail.com>
2025-05-30 18:21:41 +1000

    SIT-000: Remove Integration Tests

--- short-it/short-it-api/UrlShortener.IntegrationTest/Constants/TestConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace UrlShortener.IntegrationTest.Constants;
-
-public static class TestConstants
-{
-    public const int UrlId1 = 1;
-    public const string UrlShortPrefix1 = "MDAwMDA";
-    public const string OriginalUrl1 = "https://netflix.com";
-}
